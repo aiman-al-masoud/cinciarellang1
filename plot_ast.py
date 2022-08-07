0422df520c2ddff2b049c9a7eb0d4b9d1296a222
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-07 17:55:53 +0200

    ...

--- plot_ast.py
+++ plot_ast.py
@@ -98,7 +98,7 @@ def main():
     # s = "1;"
     s = "1+1; 2+2; 3*3+4;"
     s = "if x {y = 1;}else{y = 2; y = y + 1; };"
-    s = "if x {}"
+    # s = "if x {}"
 
 
     p = Parser(TokenStream(CharStream(s)))
