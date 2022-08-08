80ce7b68d160579f860bf89f6975256b5197e7d1
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-08 15:10:04 +0200

    ...

---
+++ enviro.py
@@ -0,0 +1,31 @@
+class Enviro:
+
+    def __init__(self, parent:'Enviro'):
+        self.vars:dict = (parent or {}) and parent.vars 
+
+    def new_child(self):
+        return Enviro(self)
+
+    def get(self, key:str):
+        return self.vars[key]
+
+    def set(self, key:str, val):
+        self.vars[key] = val
+
+    def __repr__(self):
+        return str(self.vars)
+
+
+def main():
+    e = Enviro(None)
+    e.set('x', 1)
+    e.set('y', 1)
+
+    v = e.new_child()
+    # print(e)
+    print(v)
+    v.set('x', 'capra')
+    print(v)
+
+if __name__ == "__main__":
+    main()
