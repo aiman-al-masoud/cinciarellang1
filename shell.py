c479063e7e61238f99d72d10f434a8e7bdef03af
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-08 16:23:27 +0200

    ...

--- shell.py
+++ shell.py
@@ -3,5 +3,16 @@ from token_stream import TokenStream
 from parser import Parser
 from interpreter import Interpreter
 
+# REPL
+while True:
 
+    s = "true || false;"
+    p = Parser(TokenStream(CharStream(s)))
+    prg = p.parse()
+
+
+    i = Interpreter()
+    r = i.eval(prg[0])
+
+    print(r)
 
