89876296dec8c18313d12ca0f27a17becec9780a
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-08 22:58:55 +0200

    fixed recursive functions, yay!

--- fun.py
+++ fun.py
@@ -2,9 +2,7 @@ from enviro import Enviro
 
 class Fun:
 
-    def __init__(self, params, body, _eval): #, parent: Enviro, _eval):
-        # self.env = parent.new_child()
-        # print("fun's env:", self.env)
+    def __init__(self, params, body, _eval):
         self.params = params
         self.body = body
         self.eval = _eval
@@ -14,15 +12,9 @@ class Fun:
             env.set(self.params[i]['val'], self.eval(arg))
     
     def run (self, args, env):
-        # global x 
-        # print("call with args:", args)
-        # if x > 10:
-            # exit()
-        # x +=1
         self.bind_args(args, env)
         return self.eval(self.body)
 
-# x = 1
 
 # new env should be created from parent env at function's runtime,
 # or else recursive calls to the function will have trouble reaching the 
