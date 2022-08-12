143be51f09c8854e77c90de1a6366cda13375732
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-12 14:57:48 +0200

    ...

--- src/to_py.py
+++ src/to_py.py
@@ -19,10 +19,40 @@ class ToPy:
             rv = self.eval(ast['right'])
             return f"{lv} = {rv}"
 
+        if ast['type'] in ['add', 'sub', 'mul', 'div', 'or', 'and', '==', '!=', '>', '<', '>=', '<=']:
+            return self.apply_op(ast['type'], self.eval(ast['left']), self.eval(ast['right']))
+
+    def apply_op(self, op:str, left, right):
+        
+        if op == 'add':
+            return f"{left} + {right}"
+        if op == 'sub':
+            return f"{left} - {right}"
+        if op == 'mul':
+            return f"{left} * {right}"
+        if op == 'div':
+            return f"{left} / {right}"
+        if op == 'or':
+            return f"{left} or {right}"
+        if op == 'and':
+            return f"{left} and {right}"
+        if op == '==':
+            return f"{left} == {right}"
+        if op == '!=':
+            return f"{left} != {right}"
+        if op == '>':
+            return f"{left} > {right}"
+        if op == '<':
+            return f"{left} < {right}"
+        if op == '>=':
+            return f"{left} >= {right}"
+        if op == '<=':
+            return f"{left} <= {right}"
+
 
 
 def main():
-    s = "x = true;"
+    s = "x = (1 > 1) || true;"
     ast = Parser(TokenStream(CharStream(s))).parse()[0]
     ps = ToPy().eval(ast)
     print(ps)
